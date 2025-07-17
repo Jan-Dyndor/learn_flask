c02dea6e4488a5cda391de29a56ec7a4d069b615
Jan Dyndor <dyndorjan@gmail.com>
2025-07-17 20:30:20 +0200

    added simple test on home page

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,1 +1,8 @@
 from app import app
+
+
+def test_homepage_200():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert "hello" in response.data.decode("utf-8")
