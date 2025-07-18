dec30cc740ee543c955979d78d6ce413aea5b5be
Jan Dyndor <dyndorjan@gmail.com>
2025-07-18 17:53:31 +0200

    learn variables

--- app/routes.py
+++ app/routes.py
@@ -4,3 +4,18 @@ from app import app
 @app.route("/")
 def home():
     return "hello"
+
+
+@app.route("/<name>")
+def welcome_name(name):
+    return "Welcome %s" % name
+
+
+@app.route("/blog/<int:postID>")
+def show_blog_post(postID):
+    return "PostID: %d" % postID
+
+
+@app.route("/rev/<float:rev>")
+def show_rev(rev):
+    return "Movie rating is: %f" % rev
