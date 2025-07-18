d40c813075da5d38ed5dd4c0e3ef06466ce509ca
Jan Dyndor <dyndorjan@gmail.com>
2025-07-18 18:19:29 +0200

    passed tests for variables learning in flask

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,8 +1,27 @@
 from app import app
 
 
-def test_homepage_200():
+def test_homepage_get():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
     assert "hello" in response.data.decode("utf-8")
+
+
+def test_string_var():
+    client = app.test_client()
+    response = client.get("/Jan")
+    assert response.status_code == 200
+    assert "Jan" in response.data.decode("utf-8")
+
+
+def test_int_var():
+    client = app.test_client()
+    response = client.get("/blog/24")
+    assert response.status_code == 200
+
+
+def test_float_var():
+    client = app.test_client()
+    response = client.get("/rev/3.4")
+    assert response.status_code == 200
